33ba20b033d441b3af2a90308e3c1084dfe53835
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-09 23:08:45 -0400

    stuck on model

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,17 +1,43 @@
-const getAll = () => {
-    return 'getAll';
+const db = require('../../data/db-config');
+// const getAll = () => {
+//     return 'getAll';
+// };
+
+const getById = (recipe_id) => {
+    const bob = db('recipes')
+        .select('rs.step_id', 'rs.step_num', 'rs.step_instructions', 'i.*')
+        .leftJoin('recipe_steps as rs', 'r.recipe_id', '=', 'rs.step_id')
+        .join('step_ingredients as si', 'rs.step_id', '=', 'si.step_id')
+        .join('ingredients as i', 'si.ingredient_id', '=', 'i.ingredient_id')
+        .where('r.recipe_id', recipe_id)
+        .groupBy('r.recipe_id')
+        .orderBy('r.recipe_id');
+    console.log(bob);
+
+    return bob;
+
 };
 
-const getById = () => {
-    return 'getById';
-};
+// select r.*, 
+// rs.step_id, 
+// rs.step_num, 
+// rs.step_instructions,
+// i.*
+// from recipes as r
+// left join recipe_steps as rs on r.recipe_id = rs.step_id
+// join step_ingredients as si on rs.step_id = si.step_id
+// join ingredients as i on si.ingredient_id = i.ingredient_id
+// where r.recipe_id = rs.recipe_id
 
-const create = () => {
-    return 'create';
-};
+// group by r.recipe_id
+// order by r.recipe_id
+
+
+
+// const create = () => {
+//     return 'create';
+// };
 
 module.exports = {
-    getAll,
-    getById,
-    create
+    getById
 };
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,17 +1,18 @@
 const express = require('express');
-//const Recipe = require('./recipes-model');
+const Recipe = require('./recipes-model');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    try {
-        res.json('GET all is Working');
-    } catch (err) {
-        next(err);
-    }
-});
+// router.get('/', (req, res, next) => {
+//     try {
+//         res.json('GET all is Working');
+//     } catch (err) {
+//         next(err);
+//     }
+// });
 
 router.get('/:id', (req, res, next) => {
     try {
+        Recipe.getById(req.body);
         res.json('GET id is Working');
     } catch (err) {
         next(err);
